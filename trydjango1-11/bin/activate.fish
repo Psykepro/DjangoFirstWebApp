72001eb56bfb6099dfa1b30e298d286f124a2280
Psykepro <nikolai_nedkov@yahoo.com>
2017-09-28 04:40:25 -0400

    Adding changes in home view and fixing virtual environment.

--- trydjango1-11/bin/activate.fish
+++ trydjango1-11/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/koleca/Dev/trydjango1-11"
+set -gx VIRTUAL_ENV "/home/koleca/Dev/DjangoFirstWebApp/trydjango1-11"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- trydjango1-11/src/restaurants/views.py
+++ trydjango1-11/src/restaurants/views.py
@@ -4,5 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 # Function based view
 def home(request):
-	return HttpResponse("hello")
-	# return render(request, "home.html", {})
+    html_var = 'html'
+    html = f"""<!DOCTYPE html><html lang="en"><head></head><body><h1> HALLA </h1><p>This is {html_var} coming through</p></body></html>"""
+    return HttpResponse(html) 
+    # return render(request, "home.html", {}
