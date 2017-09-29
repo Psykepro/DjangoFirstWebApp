c15e106e220475fe843f392f18d66cd6814499f7
Psykepro <nikolai_nedkov@yahoo.com>
2017-09-29 06:19:30 -0400

    Rendering HTML

--- trydjango1-11/src/restaurants/views.py
+++ trydjango1-11/src/restaurants/views.py
@@ -1,10 +1,9 @@
+import random
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 # Function based view
 def home(request):
-    html_var = 'html'
-    html = f"""<!DOCTYPE html><html lang="en"><head></head><body><h1> HALLA </h1><p>This is {html_var} coming through</p></body></html>"""
-    return HttpResponse(html) 
-    # return render(request, "home.html", {}
+    num = random.randint(0, 10000000)
+    return render(request, "base.html", {"html_var":"context variable", "num": num})
