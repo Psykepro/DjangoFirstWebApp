0002e5da04773cbe852ac4724da99a97902536f1
Psykepro <nikolai_nedkov@yahoo.com>
2017-09-29 07:30:22 -0400

    Render a Django Template, Context in Django Templates, Template Inheritance

--- trydjango1-11/src/restaurants/views.py
+++ trydjango1-11/src/restaurants/views.py
@@ -4,6 +4,28 @@ from django.http import HttpResponse
 
 # Create your views here.
 # Function based view
+def contact(request):
+    context = {
+    }
+    return render(request, "contact.html", context)
+
+def about(request):
+    context = {
+    }
+    return render(request, "about.html", context)
+
 def home(request):
-    num = random.randint(0, 10000000)
-    return render(request, "base.html", {"html_var":"context variable", "num": num})
+    num = None
+    some_list = [
+        random.randint(0, 10000000),
+        random.randint(0, 10000000),
+        random.randint(0, 10000000)
+    ]
+    conditional_bool_item = True
+    if conditional_bool_item:
+        num = random.randint(0, 10000000)
+    context = {
+            "num": num,
+            "some_list":some_list
+    }
+    return render(request, "home.html", context)
