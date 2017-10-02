8b22fbd263721ba8ce3a6b75ad6a6cb6795b00cb
Psyke <psykepro@abv.bg>
2017-10-02 20:02:04 +0300

    Generic List View

--- trydjango1-11/src/restaurants/views.py
+++ trydjango1-11/src/restaurants/views.py
@@ -1,8 +1,9 @@
 import random
+from django.db.models import Q
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 # Create your views here.
 # Function based view
 from .models import RestaurantLocation
@@ -14,3 +15,18 @@ def restaurant_list_view(request):
         'object_list': queryset
     }
     return render(request, template_name, context)
+
+class RestaurantListView(ListView):
+    template_name = 'restaurants/restaurants_list.html'
+
+    def get_queryset(self):
+        slug = self.kwargs.get('slug')
+        if slug:
+            queryset = RestaurantLocation.objects.filter(
+                    Q(category__iexact=slug) |
+                    Q(category__icontains=slug)
+                )
+        else:
+            queryset = RestaurantLocation.objects.all()
+
+        return queryset
