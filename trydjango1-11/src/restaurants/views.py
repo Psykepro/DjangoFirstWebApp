b13a725120956d56d803a60e3d44c0b4aa88c443
Psyke <psykepro@abv.bg>
2017-10-02 17:39:16 +0300

    Template View

--- trydjango1-11/src/restaurants/views.py
+++ trydjango1-11/src/restaurants/views.py
@@ -2,41 +2,29 @@ import random
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-
+from django.views.generic import TemplateView
 # Create your views here.
 # Function based view
 
-class ContactView(View):
-    def get(self, request, *args, **kwargs):
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        num = None
+        some_list = [
+            random.randint(0, 10000000),
+            random.randint(0, 10000000),
+            random.randint(0, 10000000)
+        ]
+        conditional_bool_item = True
+        if conditional_bool_item:
+            num = random.randint(0, 10000000)
         context = {
+                "num": num,
+                "some_list":some_list
         }
-        print(kwargs)
-        return render(request, "contact.html", context)
-
-def contact(request):
-    context = {
-    }
-    return render(request, "contact.html", context)
-
-def about(request):
-    context = {
-    }
-    return render(request, "about.html", context)
-
-def home(request):
-    num = None
-    some_list = [
-        random.randint(0, 10000000),
-        random.randint(0, 10000000),
-        random.randint(0, 10000000)
-    ]
-    conditional_bool_item = True
-    if conditional_bool_item:
-        num = random.randint(0, 10000000)
-    context = {
-            "num": num,
-            "some_list":some_list
-    }
-    return render(request, "home.html", context)
+        print(context)
+        return context
 
 
