513239218e1c65e21e85f872117bd6b0be27b574
Psykepro <nikolai_nedkov@yahoo.com>
2017-10-03 09:58:05 -0400

    SlugField & the Unique Slug Generator

--- trydjango1-11/src/restaurants/models.py
+++ trydjango1-11/src/restaurants/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 # Create your models here.
 class RestaurantLocation(models.Model):
-	name		= models.CharField(max_length=120)
-	location	= models.CharField(max_length=120, null=True, blank=True)
-	category 	= models.CharField(max_length=120, null=True, blank=True)
-	timestamp 	= models.DateTimeField(auto_now_add=True)
-	updated 	= models.DateTimeField(auto_now=True)
+    name        = models.CharField(max_length=120)
+    location    = models.CharField(max_length=120, null=True, blank=True)
+    category    = models.CharField(max_length=120, null=True, blank=True)
+    timestamp   = models.DateTimeField(auto_now_add=True)
+    updated     = models.DateTimeField(auto_now=True)
+    slug        = models.SlugField(null=True, blank=True)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
+
+    @property
+    def title(self):
+        return self.name
