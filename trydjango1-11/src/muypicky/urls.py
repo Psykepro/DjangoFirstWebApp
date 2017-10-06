36ba05c00f5f85013491656df76e11d7e7dc4401
Psykepro <nikolai_nedkov@yahoo.com>
2017-10-06 03:46:32 -0400

    Password Reset is fully functional now.

--- trydjango1-11/src/muypicky/urls.py
+++ trydjango1-11/src/muypicky/urls.py
@@ -17,7 +17,12 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-from django.contrib.auth.views import LoginView,PasswordResetView, PasswordResetDoneView
+from django.contrib.auth.views import (
+    LoginView,PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
 
 from restaurants.views import (
     restaurant_create_view,
@@ -32,8 +37,10 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^password-reset/$', PasswordResetView.as_view(), name='password_reset'),
-    url(r'^password_reset/done/$', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    url(r'^password-reset/done/$', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    url(r'^password-reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     url(r'^restaurants/', include('restaurants.urls', namespace='restaurants')),
     url(r'^about/$', TemplateView.as_view(template_name = 'about.html'), name='about'),
     url(r'^contact/$', TemplateView.as_view(template_name = 'contact.html'), name='contact'), #(?P<id>\d+)/ regex for id
+    url(r'^password-reset/complete/$', PasswordResetCompleteView.as_view(), name='password_reset_complete')
 ]
